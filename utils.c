ad0414e11548038174f4fb1f36c783b77910352d
Yoann067 <yoann.louazel@gmail.com>
2019-12-16 15:21:30 +0100

    multiply matrice maj

--- utils.c
+++ utils.c
@@ -1,14 +1,16 @@
-int[][] multiplyMatrice(int[][] tab1, int[][] tab2){
-    if(tab1.length == tab2.length && tab1[0].length == tab2[0].length){
-        int dimx = tab1.length;
-        int dimy = tab1[0].length;
-        int[dimx][dimy] mul;
-        for(int i = 0; i < dimx; i++)
+int* multiplyMatrice(int* tab1, int* tab2){
+    if(tab1.length==tab2[0].length){
+        int dim1x = tab1.length;
+        int dim1y = tab1[0].length;
+        int dim2x = tab1.length;
+        int dim2y = tab1[0].length;
+        int mul[dimx][dimy];
+        for(int i = 0; i < dim1x; i++)
         {
-            for(int j = 0; j < dimy; j++)
+            for(int j = 0; j < dim2y; j++)
             {
                 mul[i][j]=0;
-                for(k = 0; k < dimy; k++)
+                for(int k = 0; k < dim1y; k++)
                 {
                     mul[i][j] += tab1[i][k] * tab2[k][j];
                 }
