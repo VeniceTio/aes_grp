68fdc7365a96820ef38b3975d2f50773d1793d1c
Thomas Duchmann <thomas.duchmann@etu.unistra.fr>
2019-12-16 13:55:12 +0100

    Fonctions aes vides

---
+++ aes.h
@@ -0,0 +1,5 @@
+void keyExpansion();
+void AddRoundKey();
+void SubBytes();
+void shiftRows();
+void mixColumns();
---
+++ main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "aes.h"
+#include "utils.h"
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
