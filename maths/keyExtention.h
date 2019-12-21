8740de6099969541b5ab87f0d7a65039c7f367ac
UKHANOV ILYA <ukhanov@unistra.fr>
2019-12-21 11:48:33 +0100

    Ignored Code Blocks files

--- maths/keyExtention.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-
-extern int KEY_LENGTH;
-
-char* stringToHexStr(char* text, char output[256]);
-void hexStrToKey(char* hex, int* key);
--- maths/utils.h
+++
@@ -1,6 +0,0 @@
-#include <stdint.h>
-
-void print(uint8_t* display);
-int** multiplyMatrice(int tab1[4][4], int tab2[4][4]);
-int length(uint8_t* array);
-int length2(uint8_t** array);
