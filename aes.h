b6e40107b82bc808f8315abcf0d1140740620ff6
Thomas Duchmann <thomas.duchmann@etu.unistra.fr>
2020-01-01 11:59:11 +0100

    mixColumns et invMixColumns (marche pas)

--- aes.h
+++ aes.h
@@ -6,11 +6,9 @@ extern int NB_ROUNDS;
 void subWord(uint8_t word[4]);
 
 uint8_t* subBytes(uint8_t* word);
-
 uint8_t subByte(uint8_t val);
 
 uint8_t* invSubBytes(uint8_t* word);
-
 uint8_t invSubByte(uint8_t val);
 
 uint8_t* shiftRows(uint8_t* word);
@@ -18,3 +16,6 @@ uint8_t* mixColumns(uint8_t* word);
 
 uint8_t* invShiftRows(uint8_t* word);
 uint8_t* invMixColumns(uint8_t* word);
+uint8_t* multiply(uint8_t* column);
+uint8_t* invMultiply(uint8_t* column);
+uint8_t* getColumn(uint8_t* matrice, int pos);
--- utils.h
+++ utils.h
@@ -8,6 +8,7 @@ struct HexDigits {
 
 uint8_t* xorWords(uint8_t* word1, uint8_t* word2);
 void printWord(uint8_t* word);
+void printWordHex(uint8_t* word);
 void print(uint8_t* display);
 int** multiplyMatrice(int tab1[4][4], int tab2[4][4]);
 struct HexDigits getHexDigits(uint8_t byte);
